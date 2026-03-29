6f969b02f38403b2a8003167689fc123d9057685
Luigibarte4563 <luigibarte1@gmail.com>
2026-03-29 08:14:21 +0800

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome</h1>
+    <p>click the button below to view the list of miners</p>
+    <a href="" class="btn">
+        Find miners
+    </a>
+</body>
+</html>
