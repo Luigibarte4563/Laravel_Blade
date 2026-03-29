59f0532adb6d346e5eb70a2a8820da73c2104212
Luigibarte4563 <luigibarte1@gmail.com>
2026-03-29 21:21:32 +0800

    directives

--- resources/views/luwes/index.blade.php
+++ resources/views/luwes/index.blade.php
@@ -9,17 +9,17 @@
     
     <p>{{ $greeting }}</p>
 
+    @if($greeting == "hello")
+        <p>Hi from inside the if statement</p>
+    @endif
+
     <ul>
-        <li>
-            <a href="/luwes/{{$ninjas[0]["id"]}}">
-                {{ $ninjas[0]["name"]}}
-            </a>
-        </li>
-        <li>
-            <a href="/luwes/{{$ninjas[1]["id"]}}">
-                {{ $ninjas[1]["name"]}}
-            </a>
-        </li>
+        @foreach($ninjas as $ninja)
+            <li>
+                <p>{{ $ninja['name']}}</p>
+                <a href="/ninjas/{{ $ninja['id']}} ">View details</a>
+            </li>
+        @endforeach
     </ul>
 </body>
 </html>
